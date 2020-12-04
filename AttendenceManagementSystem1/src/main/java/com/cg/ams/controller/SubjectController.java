5c64ecc52b60da573a8cdb22c9f69299a0aae8bb
Nagaraj <Nagaraj@NAGARAJ-PC>
2020-12-04 10:33:08 +0530

    log4j

--- AttendenceManagementSystem1/src/main/java/com/cg/ams/controller/SubjectController.java
+++ AttendenceManagementSystem1/src/main/java/com/cg/ams/controller/SubjectController.java
@@ -2,6 +2,7 @@ package com.cg.ams.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,59 +19,73 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cg.ams.entity.SubjectEntity;
 import com.cg.ams.exception.RecordNotFoundException;
 import com.cg.ams.service.SubjectService;
+
 /*
  * @author NagaRaj
  */
 //mark class as Controller  
 @RestController
-@RequestMapping("/api/amsattendence")
+
 public class SubjectController {
 	// autowire the SubjectService class
 	@Autowired
 	private SubjectService subjectService;
+	public static final Logger log = Logger.getLogger(SubjectController.class.getName());
 
 	@GetMapping("/hello-world")
 	public String sayHello() {
-		return "HelloWorld";		
+		return "HelloWorld";
 	}
-	//creating a get mapping that retrieves all the subjects detail from the database
-	@GetMapping("/list")
+
+	// creating a get mapping that retrieves all the subjects detail from the
+	// database
+	@GetMapping("/list-student")
 	public List<SubjectEntity> getAllSubject() throws RecordNotFoundException {
+		log.info("Started Viewing all Subject Details ");
 		ResponseEntity<Boolean> responseEntity = new ResponseEntity(true, HttpStatus.OK);
 		return subjectService.findAllSubjects();
-		
+
 	}
+
 	// creating post mapping that post the subject detail in the database
-	@PostMapping("/insert")
+	@PostMapping("/insert-student")
 	public Long create(@RequestBody SubjectEntity subject) {
+		log.info("Started Viewing  Subject Details ");
 		subjectService.add(subject);
 		@SuppressWarnings({ "unused", "unchecked", "rawtypes" })
 		ResponseEntity<Boolean> responseEntity = new ResponseEntity(true, HttpStatus.OK);
+		log.info("Completed creating Subject Details Viewed");
 		return subject.getCourseId();
 	}
 
 	// creating put mapping that updates the subject detail
-	@PutMapping("/update")
+	@PutMapping("/update-student")
 	public ResponseEntity<Boolean> update(@RequestBody SubjectEntity subject) {
+		log.info("Started updating  Subject Details ");
 		subjectService.update(subject);
 		@SuppressWarnings({ "rawtypes", "unchecked" })
 		ResponseEntity<Boolean> responseEntity = new ResponseEntity(true, HttpStatus.OK);
+		log.info("Completed updating  Subject Details ");
 		return responseEntity;
 	}
 
 	// creating a delete mapping that deletes a specified subject
-		@DeleteMapping(value = "/delete/{subjectId}")
-		public String deleteSubject(@PathVariable("subjectId") Long subjectId) throws RecordNotFoundException {
-			subjectService.deleteBySubjectId(subjectId);
-			return "subject has been deleted successfully";
-		}
-
-	// creating a get mapping that retrieves the detail of a specific subject
-	@GetMapping("/findSubject/{subjectId}")
-	public ResponseEntity<SubjectEntity> getSubjectById(@PathVariable("subjectId") Long subjectId) throws RecordNotFoundException {
-	SubjectEntity subject = subjectService.getSubjectById(subjectId);
-	return new ResponseEntity<SubjectEntity>(new HttpHeaders(), HttpStatus.OK);
+	@DeleteMapping(value = "/delete-student/{subjectId}")
+	public String deleteSubject(@PathVariable("subjectId") Long subjectId) throws RecordNotFoundException {
+		log.info("Started deleting  Subject Details ");
+		subjectService.deleteBySubjectId(subjectId);
+		log.info("Completed deleting  Subject Details ");
+		return "subject has been deleted successfully";
 	}
 
-	
+	// creating a get mapping that retrieves the detail of a specific subject
+	@GetMapping("/find-Subject/{subjectId}")
+	public ResponseEntity<SubjectEntity> getSubjectById(@PathVariable("subjectId") Long subjectId)
+			throws RecordNotFoundException {
+		log.info("Started finding subject Details By subject Id  ");
+		SubjectEntity subject = subjectService.getSubjectById(subjectId);
+		log.info("completed finding subject Details By subject Id  ");
+		return new ResponseEntity<SubjectEntity>(subject, new HttpHeaders(), HttpStatus.OK);
+	}
+
 }
--- AttendenceManagementSystem1/src/test/java/com/cg/ams/test/SubjectControllerTest.java
+++ AttendenceManagementSystem1/src/test/java/com/cg/ams/test/SubjectControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
 
 import com.cg.ams.controller.SubjectController;
 import com.cg.ams.entity.SubjectEntity;
@@ -73,7 +74,21 @@ public class SubjectControllerTest {
 		
 		assertEquals(112, subjectEntity.getCourseId());
 	}
-	
+	@Test
+public void viewSubjectByIdTest() throws RecordNotFoundException {
+		SubjectEntity subjectEntity = new SubjectEntity();
+		subjectEntity.setCourseId((long) 111);
+		subjectEntity.setCourseName("Ece");
+		subjectEntity.setSemester("VIII");
+		subjectEntity.setName("Teja");
+		subjectEntity.setSubjectId((long) 1695);
+		subjectEntity.setSubjectCode("16uecs02");
+		Long subjectId = subjectEntity.getSubjectId();
+		when(subjectController.getSubjectById(subjectId).getBody()).thenReturn(subjectEntity);
+		ResponseEntity<SubjectEntity> viewSubject = subjectController.getSubjectById(subjectId);
+		assertEquals(111, viewSubject.getBody().getCourseId());
+
+	}
 @Test
 public void viewSubjectListTest() throws RecordNotFoundException {
 	List<SubjectEntity> subjectList = new ArrayList<SubjectEntity>();
